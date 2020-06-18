04667ca8368c91e2ae3fbaa984d351c9a8c6814d
BL <iceliang@gmail.com>
2020-06-18 22:49:15 +1200

    introduced react-bootstrap and styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,34 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { ButtonToolbar, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+`;
+const StyledButton = styled(Button)`
+	padding: 3%, %5, 3%, 5%;
+`;
+const CenteredDiv = styled.div`
+	text-align: center;
+`;
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<span>
-					<span>Learn </span>
-					<a
-						className='App-link'
-						href='https://reactjs.org/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						React
-					</a>
-					<span>, </span>
-					<a
-						className='App-link'
-						href='https://redux.js.org/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						Redux
-					</a>
-					<span>, </span>
-					<a
-						className='App-link'
-						href='https://redux-toolkit.js.org/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						Redux Toolkit
-					</a>
-					,<span> and </span>
-					<a
-						className='App-link'
-						href='https://react-redux.js.org/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						React Redux
-					</a>
-				</span>
-			</header>
-		</div>
+		<CenteredDiv className='App'>
+			<StyledHeader className='App-header'>
+				<ButtonToolbar className='custom-btn-toolbar'>
+					<LinkContainer to='/componentsView'>
+						<StyledButton>View Components</StyledButton>
+					</LinkContainer>
+				</ButtonToolbar>
+			</StyledHeader>
+		</CenteredDiv>
 	);
 }
 
